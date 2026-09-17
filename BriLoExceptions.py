46bb26b181a8b25f4e261256ebb23d7ead23283d
burakcekicnl <burakcekic@gmail.com>
2026-09-17 21:49:02 +0200

    chore: add .gitignore and initialize project

---
+++ BriLoExceptions.py
@@ -0,0 +1,12 @@
+class InvalidCsvError(Exception):
+    #Fouten met betrekking tot de
+    # extensie of het bestaan van het CSV-bestand.
+    #
+    pass
+
+class MissingColumnError (Exception):
+    #Deze fout treedt op
+    #als het CSV-bestand de verwachte kolommen mist.
+    #
+    pass
+
