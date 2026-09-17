04e789394aecde7ccece1e8441e6439ae3cb82a4
burakcekicnl <burakcekic@gmail.com>
2026-09-17 23:01:19 +0200

    feat(report): add BriLoReport class structure and type hints

---
+++ report.py
@@ -0,0 +1,25 @@
+from typing import List, Dict
+
+
+class BriLoReport:
+    """ Het is de klasse die de gegevens verwerkt.
+
+        Attiributes:
+            VERPLICHTE_COLUMNS (set[str]): Vereiste kolommen die in een CSV-bestand moeten worden opgenomen.
+            file_path (str): Het pad naar het te verwerken CSV-bestand.
+    """
+    def __init__(self, orders: List[Dict[str, str]]) -> None:
+        """ Start de BriLoReader-klasse.
+        Args:
+            orders: Het pad naar het te lezen CSV-bestand.
+        """
+        self.orders_data: List[Dict[str,str]] = orders
+
+   def
+
+
+if __name__ == "__main__":
+    data=[]
+    test_reader = BriLoReport(data)
+    print("Testing...")
+
